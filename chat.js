a0b1c9ab36b400308882afa48a0603f2d6e00055
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2023-08-23 14:12:57 -0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,89 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCr9mKpbq-fcXAYMYZkni5BzpcrIwGA01g",
+    authDomain: "chat-b33ab.firebaseapp.com",
+    databaseURL: "https://chat-b33ab-default-rtdb.firebaseio.com",
+    projectId: "chat-b33ab",
+    storageBucket: "chat-b33ab.appspot.com",
+    messagingSenderId: "789744330651",
+    appId: "1:789744330651:web:7e021fa6ff1d76a5cba33e",
+    measurementId: "G-NCH2D1WBNC"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const nomeUsuario = localStorage.getItem("nomeUsuario");
+const nomeSala = localStorage.getItem("nomeSala");
+
+inicializar();
+
+function inicializar() {
+    document.getElementById("nomeSala").textContent = '#' + nomeSala;
+
+    getData();
+}
+
+function getData() {
+    const output = document.getElementById("output");
+    firebase.database().ref('/' + nomeSala).on("value", snapshot => {
+        output.innerHTML = "";
+        snapshot.forEach(childSnapshot => {
+            const childKey = childSnapshot.key;
+            if(childKey != "purpose") {
+                const childMsg = childSnapshot.val();
+                const nome = childMsg.nome;
+                const msg = childMsg.mensagem;
+                const likes = childMsg.likes;
+
+                const chatCard = document.createElement("div");
+                chatCard.className = "chatCard";
+                const chatNome = document.createElement("h4");
+                chatNome.className = "chatNome";
+                chatNome.textContent = nome;
+                chatCard.appendChild(chatNome);
+                const row = document.createElement("div");
+                row.className = "row";
+                chatCard.appendChild(row);
+                const col = document.createElement("div");
+                col.className = "col";
+                row.appendChild(col);
+                const chatMsg = document.createElement("h5");
+                chatMsg.className = "chatMsg";
+                chatMsg.textContent = msg;
+                col.appendChild(chatMsg);
+                const colAuto = document.createElement("div");
+                colAuto.className = "col-auto";
+                row.appendChild(colAuto);
+                const botaoLike = document.createElement("button");
+                botaoLike.className = "btn btn-info";
+                botaoLike.id = childKey;
+                botaoLike.value = likes;
+                botaoLike.setAttribute("onclick", "likeMsg(this.id)");
+                botaoLike.innerHTML = '<i class="fa-regular fa-thumbs-up"></i> ' + likes;
+                colAuto.appendChild(botaoLike);
+                output.appendChild(chatCard);
+            }
+        });
+    });
+}
+
+function send() {
+    const txtMsg = document.getElementById("msg");
+    const msg = txtMsg.value;
+
+    if (msg.trim()) {
+        firebase.database().ref('/' + nomeSala).push({
+            nome: nomeUsuario,
+            mensagem: msg,
+            likes: 0
+        });
+        txtMsg.value = "";
+    }
+}
+
+function likeMsg(btnId) {
+    let likes = Number(document.getElementById(btnId).value);
+    likes++;
+    firebase.database().ref('/' + nomeSala).child(btnId).update({
+        likes: likes
+    })
+}
